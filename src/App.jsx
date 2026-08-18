025a5d374f4d0dbab85d8cbf98dc68ec39ea305f
faheemdev1-gif <faheemdev1@gmail.com>
2026-08-18 18:07:30 +0500

    Insta username updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
         <a
           className="instagram-link"
-          href="https://instagram.com/"
+          href="https://instagram.com/lunaperfumes.official"
           target="_blank"
           rel="noreferrer"
         >
